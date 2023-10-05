6cbb575e0c2d38ff0e4e25a6e6b81ab5df2f17ac
sarthakw001 <sarthakw01@gmail.com>
2023-10-05 20:30:44 +0530

    Java Assignment added

--- CoreJava/src/day_1/Avg.java
+++ CoreJava/src/day_1/Avg.java
@@ -4,13 +4,17 @@ class Avg{
 	public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
 	if(sc.hasNextDouble()){
-		double x = sc.nextDouble();
-		double y = sc.nextDouble();
-		System.out.println("Average is : "+ (x+y)/2);
+	    double x = sc.nextDouble();
+	    if(sc.hasNextouble()){
+	        double y = sc.nextDouble();
+	        System.out.println("Average is : " +(x+y)/2);
+	    }else {
+	        System.out.println("Entered value is Illegal");
+	    }
 	}
 	else{
 		System.out.println("Entered value is Illegal");
 	}
-    }
+	sc.close();
 }
-
+}
