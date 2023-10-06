bb21f0373845e9ae0f163e6a509f39de664d2008
sarthakw001 <sarthakw01@gmail.com>
2023-10-06 23:00:36 +0530

    Assignment Day_2 Submission

---
+++ CoreJava/src/day_2/Point2D.java
@@ -0,0 +1,27 @@
+import java.lang.Math;
+class Point2D{
+private int x;
+private int y;
+
+Point2D(int x,int y){
+	this.x = x;
+	this.y = y;
+}
+public String show(){
+	return "X : "+ this.x + " Y : "+ this.y;
+}
+
+public boolean isEqual(Point2D point){
+	if(this.x == point.x && this.y == point.y) 
+		return true;
+	else return false; 
+}
+public double calculateDistance(Point2D point){
+	double xValue =  Math.pow((this.x-point.x),2); 
+	double yValue =  Math.pow((this.y-point.y),2);
+	
+	double distance = Math.sqrt((xValue+yValue));
+	
+	return distance;
+}
+}
