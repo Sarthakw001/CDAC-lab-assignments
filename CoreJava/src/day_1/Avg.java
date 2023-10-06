6fb6d18aa2029cf209b18a863505491a7a124481
sarthakw001 <sarthakw01@gmail.com>
2023-10-06 23:05:35 +0530

    Assignment Day_2 Submission

--- CoreJava/src/day_1/Avg.java
+++ CoreJava/src/day_1/Avg.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
-class Avg{
-	public static void main(String[] args){
-	Scanner sc = new Scanner(System.in);
-	if(sc.hasNextDouble()){
-	    double x = sc.nextDouble();
-	    if(sc.hasNextouble()){
-	        double y = sc.nextDouble();
-	        System.out.println("Average is : " +(x+y)/2);
-	    }else {
-	        System.out.println("Entered value is Illegal");
-	    }
+class Avg {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		if (sc.hasNextDouble()) {
+			double x = sc.nextDouble();
+			if (sc.hasNextDouble()) {
+				double y = sc.nextDouble();
+				System.out.println("Average is : " + (x + y) / 2);
+			} else {
+				System.out.println("Entered value is Illegal");
+			}
+		} else {
+			System.out.println("Entered value is Illegal");
+		}
+		sc.close();
 	}
-	else{
-		System.out.println("Entered value is Illegal");
-	}
-	sc.close();
-}
 }
