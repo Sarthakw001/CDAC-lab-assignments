37b5ce909d48b01530115c7a7ddaea912e4a8fe4
dac <dac@students.acts.in>
2023-12-24 13:00:38 +0530

    s

---
+++ dot_net/StudentManagementMVC/Controllers/StudentController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagementMVC.Models;
+
+namespace StudentManagementMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: StudentController
+        public ActionResult Index()
+        {
+
+            List<Student> list = Student.GetAllStudent();
+
+            return View(list);
+    
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            Student obj = Student.GetSingleStudent(id);
+            return View(obj);
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student obj, IFormCollection collection, int StudentNo, string Name, string Section, string Branch, string EmailId)
+        {
+            try
+            {
+
+                //to do - add to db
+                Student.Insert(obj);
+                //return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index");
+                ViewBag.message = "success";
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.message = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Student obj = Student.GetSingleStudent(id);
+            return View(obj);
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Student obj)
+        {
+            try
+            {
+                Student.Update(obj);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Student obj = Student.GetSingleStudent(id);
+            return View(obj);
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(IFormCollection collection, int id, Student obj)
+        {
+            try
+            {
+                Student.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ dot_net/StudentManagementMVC/Models/Student.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace StudentManagementMVC.Models
+{
+    public class Student
+    {
+
+        public int StudentNo { get; set; }
+        public string? Name { get; set; }
+        public string? Section { get; set; }
+        public string? Branch { get; set; }
+
+        public string? EmailId { get; set; }
+        public static void Insert(Student obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StudentDB;Integrated Security=True";
+            try
+            {
+                cn.Open();
+                SqlCommand cmdInsert = new SqlCommand();
+                cmdInsert.Connection = cn;
+                cmdInsert.CommandType = System.Data.CommandType.Text;
+                cmdInsert.CommandText = "insert into tbl_student values(@StudentNo,@Name,@Section,@Branch,@EmailId)";
+
+
+
+                cmdInsert.Parameters.AddWithValue("@StudentNo", obj.StudentNo);
+                cmdInsert.Parameters.AddWithValue("@Name", obj.Name);
+                cmdInsert.Parameters.AddWithValue("@Section", obj.Section);
+                cmdInsert.Parameters.AddWithValue("@Branch", obj.Branch);
+                cmdInsert.Parameters.AddWithValue("@EmailId", obj.EmailId);
+                cmdInsert.ExecuteNonQuery();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
+        public static void Update(Student obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StudentDB;Integrated Security=True";
+            try
+            {
+                cn.Open();
+                SqlCommand cmdInsert = new SqlCommand();
+                cmdInsert.Connection = cn;
+                cmdInsert.CommandType = System.Data.CommandType.Text;
+                cmdInsert.CommandText = "update tbl_student set Name=@Name, Section=@Section, Branch=@Branch ,EmailId=@EmailId where StudentNo=@StudentNo";
+
+
+
+
+                cmdInsert.Parameters.AddWithValue("@StudentNo", obj.StudentNo);
+                cmdInsert.Parameters.AddWithValue("@Name", obj.Name);
+                cmdInsert.Parameters.AddWithValue("@Section", obj.Section);
+                cmdInsert.Parameters.AddWithValue("@Branch", obj.Branch);
+                cmdInsert.Parameters.AddWithValue("@EmailId", obj.EmailId);
+                cmdInsert.ExecuteNonQuery();
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+
+
+        }
+        public static void Delete(int StudentNo)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StudentDB;Integrated Security=True";
+            try
+            {
+                cn.Open();
+                SqlCommand cmdInsert = new SqlCommand();
+                cmdInsert.Connection = cn;
+                cmdInsert.CommandType = System.Data.CommandType.Text;
+                cmdInsert.CommandText = "delete from tbl_student where StudentNo =@StudentNo";
+                cmdInsert.Parameters.AddWithValue("@StudentNo", StudentNo);
+                cmdInsert.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+
+
+
+        }
+        public static Student GetSingleStudent(int StudentNo)
+        {
+            Student obj = new Student();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StudentDB;Integrated Security=True";
+            try
+            {
+                cn.Open();
+                SqlCommand cmdInsert = new SqlCommand();
+                cmdInsert.Connection = cn;
+                cmdInsert.CommandType = System.Data.CommandType.Text;
+                cmdInsert.CommandText = "select * from tbl_student where StudentNo=@StudentNo";
+                cmdInsert.Parameters.AddWithValue("@StudentNo", StudentNo);
+                SqlDataReader dr = cmdInsert.ExecuteReader();
+                if (dr.Read())
+                {
+                    obj.StudentNo = dr.GetInt32("StudentNo");
+                    obj.Name = dr.GetString("Name");
+                    obj.Section = dr.GetString("Section");
+                    obj.@Branch = dr.GetString("@Branch");
+                    obj.EmailId = dr.GetString("EmailId");
+                }
+                else
+                {
+                    obj = null;
+                    //record not present
+                }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return obj;
+        }
+        public static List<Student> GetAllStudent()
+        {
+            List<Student> lstStud = new List<Student>();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StudentDB;Integrated Security=True";
+            try
+            {
+                cn.Open();
+                SqlCommand cmdInsert = new SqlCommand();
+                cmdInsert.Connection = cn;
+                cmdInsert.CommandType = System.Data.CommandType.Text;
+                cmdInsert.CommandText = "select * from tbl_student ";
+                SqlDataReader dr = cmdInsert.ExecuteReader();
+                while (dr.Read()) 
+                    lstStud.Add(new Student { StudentNo = dr.GetInt32(0), Name = dr.GetString(1), Section = dr.GetString(2), Branch = dr.GetString(3), EmailId = dr.GetString(4) });
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return lstStud;
+        }
+    }
+}
